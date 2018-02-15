8f1022812ac3fce0742b2d5c60ee5463e533c91b
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-15 01:01:42 -0800

    Removed tests

--- src/main/java/io/github/cshadd/fetch_bot/FetchBot.java
+++ src/main/java/io/github/cshadd/fetch_bot/FetchBot.java
@@ -1,9 +1,6 @@
 package io.github.cshadd.fetch_bot;
-import io.github.cshadd.cshadd_java_data_structures.util.*;
 
 public class FetchBot {
     public static void main(String[] args) {
-        BinarySearchTree<Integer> t;
-        System.out.println("Testy.");
     }
 }
