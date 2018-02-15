5bc0707a66d103c888d05715fb0ce31880ba141e
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-15 00:55:42 -0800

    Linux installer support

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/bin
+cp -r ./src/www /var/www/html/FetchBot
+mvn package
+cp ./target/fetch-bot-0.1.0.jar ~/bin/fetch-bot-0.1.0.jar
+cp ./src/start.sh ~/bin/StartFetchBot.sh
---
+++ src/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar ~/bin/fetch-bot-0.1.0.jar
+xinit /usr/bin/midori -e Fullscreen -a http://127.0.0.1/FetchBot/face.html
