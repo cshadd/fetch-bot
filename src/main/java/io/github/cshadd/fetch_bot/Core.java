020aa0aeec73ee6428bed57d2962151d3f27c568
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-20 08:50:47 -0800

    Updated jar entry point

--- src/main/java/io/github/cshadd/fetch_bot/Core.java
+++ src/main/java/io/github/cshadd/fetch_bot/Core.java
@@ -3,6 +3,10 @@ package io.github.cshadd.fetch_bot;
 // Main
 public class Core
 implements FetchBot {
+    // Private Final Instance/Property Fields
+    private final Pathfinding pathfind = new Pathfinding();
+
+    // Entry Point
     public static void main(String[] args) {
 
     }
