445fc3412968d1bf69f44233cf6a40293654d6e5
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-20 14:55:29 -0800

    Fixed web interface startup

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -4,7 +4,3 @@ export DISPLAY=:0.0
 java -jar ~/bin/fetch-bot-0.1.0.jar
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
 unclutter
-xset q
-xset s off
-xset s noblank
-xset -dpms
