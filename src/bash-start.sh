cd75c95d1585a34aa2d3e4296d7b7d806c81ccff
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-20 15:08:35 -0800

    Update bash-start.sh

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DISPLAY=:0.0
-java -jar ~/bin/fetch-bot-0.1.0.jar
+java -jar ~/bin/fetch-bot-v0.3.0.jar
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
 unclutter
