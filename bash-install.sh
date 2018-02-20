f1e62e4f050dab171731c9820ea31f350542629a
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-20 15:09:18 -0800

    Update bash-install.sh

--- bash-install.sh
+++ bash-install.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/bin
 mvn package
-cp ./target/fetch-bot-0.1.0.jar ~/bin/fetch-bot-0.1.0.jar
+cp ./target/fetch-bot-v0.3.0.jar ~/bin/fetch-bot-v0.3.0.jar
 cp ./src/bash-start.sh ~/bin/StartFetchBot.sh
 sudo chmod 764 ~/bin/StartFetchBot.sh
 sudo cp -r ./src/www /var/www/html/FetchBot
