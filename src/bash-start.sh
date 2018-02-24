26416cb4bc0d3779bc8fc5aa020c11bbdb19cf06
cshadd <christian.v.t.shadd@gmail.com>
2018-02-24 01:22:45 -0800

    Update version and info

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-LOCATION="localhost"
-
 export DISPLAY=:0.0
-java -jar ~/bin/fetch-bot-v0.4.2.jar &
-/usr/bin/chromium-browser --incognito --start-maximized --kiosk http://$LOCATION/FetchBot/face.html
+java -jar ~/bin/fetch-bot-v0.5.0.jar &
+/usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
 unclutter
