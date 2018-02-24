41aaf9b5e92649b79260745e537614b8220724c2
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-02-24 01:29:38 -0800

    Fixed uninstaller

--- bash-uninstall.sh
+++ bash-uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo rm -f -r /var/www/html/FetchBot
-find -name . 'fetch-bot*' -exec rm -f -r {} \;
+find . -name 'fetch-bot*' -exec rm -f -r {} \;
 rm -f -r ~/bin/StartFetchBot.sh
 rm -f -r ~/bin/StopFetchBot.sh
