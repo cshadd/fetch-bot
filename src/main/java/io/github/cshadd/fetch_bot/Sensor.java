23172fda1106921924b24427257d6909e2804df6
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-03-25 15:26:18 -0700

    Sensor draft

---
+++ src/main/java/io/github/cshadd/fetch_bot/Sensor.java
@@ -0,0 +1,35 @@
+package io.github.cshadd.fetch_bot;
+
+// Main
+public class Sensor
+implements FetchBot {
+    // Public Constant Instance/Property Fields
+    public enum DIRECTION {
+        FRONT,
+        LEFT,
+        RIGHT
+    }
+
+    // Public Constructors
+    public Sensor() { }
+
+    // Public Methods
+    public int getDistance(DIRECTION direction)
+    throws SensorException {
+        if (direction == DIRECTION.FORWARD) {
+            return -1;
+        }
+        else if (direction == DIRECTION.LEFT) {
+            return -1;
+        }
+        else if (direction == DIRECTION.RIGHT) {
+            return -1;
+        }
+        else if (direction == DIRECTION.STOP) {
+            return -1;
+        }
+        else {
+            throw new SensorException("Invalid direction type, " + command + ".");
+        }
+    }
+}
---
+++ src/main/java/io/github/cshadd/fetch_bot/SensorException.java
@@ -0,0 +1,10 @@
+package io.github.cshadd.fetch_bot;
+
+// Main
+public class SensorException
+implements FetchBot {
+    // Public Constructors
+    public SensorException(String message) {
+        super(message);
+    }
+}
