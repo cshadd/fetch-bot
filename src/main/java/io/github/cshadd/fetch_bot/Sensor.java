5f9b3f7e3f83add43be8ced8f01ba3758f076d74
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-03-25 15:50:02 -0700

    Fixed code

--- src/main/java/io/github/cshadd/fetch_bot/Sensor.java
+++ src/main/java/io/github/cshadd/fetch_bot/Sensor.java
@@ -16,16 +16,13 @@ implements FetchBot {
     // Public Methods
     public int getDistance(DIRECTION direction)
     throws SensorException {
-        if (direction == DIRECTION.FORWARD) {
+        if (direction == DIRECTION.FRONT) {
             return -1;
         }
         else if (direction == DIRECTION.LEFT) {
             return -1;
         }
         else if (direction == DIRECTION.RIGHT) {
-            return -1;
-        }
-        else if (direction == DIRECTION.STOP) {
             return -1;
         }
         else {
