1f698e7131b4d4eed4364ffef2e5e80c9fe8ea0e
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-03-27 09:17:17 -0700

    Fixed Sensors

---
+++ src/main/java/io/github/cshadd/fetch_bot/SensorsException.java
@@ -0,0 +1,11 @@
+package io.github.cshadd.fetch_bot;
+
+// Main
+public class SensorsException
+extends Exception
+implements FetchBot {
+    // Public Constructors
+    public SensorsException(String message) {
+        super(message);
+    }
+}
