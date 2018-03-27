d57d4e26701aaa9a30376682f6b70e28c004a912
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-03-26 19:50:12 -0700

    Migration to v0.12.0

--- bash-install.sh
+++ bash-install.sh
@@ -9,7 +9,7 @@ cd ..
 mkdir -p ~/bin
 mvn package
 cp -r ./target/libs ~/bin
-cp ./target/fetch-bot-v0.11.0.jar ~/bin/fetch-bot-v0.11.0.jar
+cp ./target/fetch-bot-v0.12.0.jar ~/bin/fetch-bot-v0.12.0.jar
 cp ./src/bash-start.sh ~/bin/StartFetchBot.sh
 cp ./src/bash-stop.sh ~/bin/StopFetchBot.sh
 sudo chmod 764 ~/bin/StartFetchBot.sh
--- src/bash-start.sh
+++ src/bash-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DISPLAY=:0.0
-sudo java -jar ~/bin/fetch-bot-v0.11.0.jar "v0.11.0" &
+sudo java -jar ~/bin/fetch-bot-v0.12.0.jar "v0.12.0" &
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
 unclutter
