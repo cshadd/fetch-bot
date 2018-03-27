9faeac2c8ab96470f4783ef358017277564a429a
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-03-27 10:14:09 -0700

    Fixed installer

--- bash-install.sh
+++ bash-install.sh
@@ -11,9 +11,7 @@ mvn package
 cp -r ./target/libs ~/bin
 cp ./target/fetch-bot-v0.12.0.jar ~/bin/fetch-bot-v0.12.0.jar
 cp ./src/bash-start.sh ~/bin/StartFetchBot.sh
-cp ./src/bash-stop.sh ~/bin/StopFetchBot.sh
 sudo chmod 764 ~/bin/StartFetchBot.sh
-sudo chmod 764 ~/bin/StopFetchBot.sh
 sudo cp -R ./src/www /var/www/html/FetchBot
 sudo chown -R $USER:www-data /var/www/html/FetchBot
 sudo chmod -R 776 /var/www/html/FetchBot
