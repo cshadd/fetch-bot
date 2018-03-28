a2309d330822ac9ad0b96cb571af985eee578cef
cshadd <christian.v.t.shadd@gmail.com>
2018-03-28 03:45:07 +0000

    Fixed installer

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -7,4 +7,4 @@ sudo xset s noblank
 sudo pkill java
 sudo java -jar ~/bin/fetch-bot-v0.12.0.jar "v0.12.0" &
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
-unclutter &
+unclutter
