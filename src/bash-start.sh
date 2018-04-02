3c842af1e575c4d9729c474d19805b01085fd73a
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-04-02 16:37:44 -0700

    Fixed core

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export DISPLAY=:0
-sudo xset -display :0 s off
-sudo xset -display :0 -dpms
-sudo xset -display :0 s noblank
+xset -display :0 s off
+xset -display :0 -dpms
+xset -display :0 s noblank
 sudo pkill java
 sudo java -jar ~/bin/fetch-bot-v0.12.0.jar "v0.12.0" &
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
