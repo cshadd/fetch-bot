3c01f0bcd5c29ad10270ec00961952c34d9bea43
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-04-02 18:34:32 -0700

    Fixed installers

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -5,6 +5,6 @@ xset -display :0 s off
 xset -display :0 -dpms
 xset -display :0 s noblank
 sudo pkill java
-sudo java -jar ~/bin/fetch-bot-v0.12.0.jar "v0.12.0" &
+sudo java -jar ~/bin/fetch-bot-v0.13.0.jar "v0.13.0" &
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
 unclutter
