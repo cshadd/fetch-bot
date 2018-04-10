c9105dff0b9ec3f275c5bfe470cfe2b3c2c350bb
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-04-10 06:35:22 -0700

    Fixed installer

--- bash-install.sh
+++ bash-install.sh
@@ -8,7 +8,7 @@ rm -f -r ./build-uno
 cd ..
 cd ..
 mkdir -p ~/bin
-mvn package
+mvn clean package
 cp -r ./target/libs ~/bin
 cp ./target/fetch-bot-v0.14.0.jar ~/bin/fetch-bot-v0.14.0.jar
 cp ./src/bash-start.sh ~/bin/StartFetchBot.sh
