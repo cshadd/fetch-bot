2e9a2d724492c271d3568dc995e22982595b4caf
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-04-26 12:19:34 -0700

    Fixed references

--- src/main/java/io/github/cshadd/fetch_bot/Core.java
+++ src/main/java/io/github/cshadd/fetch_bot/Core.java
@@ -11,7 +11,7 @@ import io.github.cshadd.fetch_bot.util.VersionCheckException;
 // Main
 public class Core
 implements FetchBot {
-    // Private Constant Instance/Property Fields
+    // Private Static Instance/Property Fields
     private static ArduinoCommunication arduinoComm;
     private static WebInterfaceCommunication webInterfaceComm;
 
