eef0fbcd613c491d700aa9fc1e88de08cdea8aa4
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-05-02 21:54:43 -0700

    Updated logger

--- src/main/java/io/github/cshadd/fetch_bot/util/Logger.java
+++ src/main/java/io/github/cshadd/fetch_bot/util/Logger.java
@@ -5,10 +5,13 @@ import io.github.cshadd.fetch_bot.FetchBot;
 import io.github.cshadd.fetch_bot.io.WebInterfaceCommunication;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.SimpleFormatter;
 import java.util.TimeZone;
 import org.apache.commons.io.FileUtils;
 
@@ -18,14 +21,17 @@ public class Logger
 implements FetchBot {
     // Private Constant Instance/Property Fields
     private static final String TAG = "[FETCH BOT]";
-
+    
     // Public Constant Instance/Property Fields
     public static final String LOG_PATH = "FetchBot.log";
-
+    
     // Private Instance/Property Fields
-    private static boolean debug;
-    private static WebInterfaceCommunication webInterfaceComm;
     private static Console console;
+    private static boolean debug;
+    private static final java.util.logging.Logger javaLogger;
+    private static FileHandler javaLoggerHandler;
+    private static SimpleFormatter javaLoggerHandlerFormatter;
+    private static WebInterfaceCommunication webInterfaceComm;
 
     // Private Constructors
     private Logger() { }
@@ -37,8 +43,7 @@ implements FetchBot {
         String returnData = FileUtils.readFileToString(input, "UTF-8");
         return returnData;
     }
-    private static void write(String msg, boolean append) {
-        File input = null;
+    private static void write(Level level, String msg, boolean append, Throwable error) {
         SimpleDateFormat localTime = null;
         SimpleDateFormat time = null;
         try {
@@ -46,19 +51,22 @@ implements FetchBot {
             localTime = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
             time.setTimeZone(TimeZone.getTimeZone("GMT"));
             msg = localTime.parse(time.format(new Date())) + " " + msg + "\n";
-            input = new File(LOG_PATH);
-            FileUtils.writeStringToFile(input, msg, "UTF-8", append);
+            javaLogger.setLevel(level);
+            javaLogger.log(level, msg, error);
         }
         catch (Exception e) {
             fatalError(e, "There was an unknown issue!");
         }
         finally { }
     }
-    private static void writeInterface(String msg, boolean append) {
-        write(msg, append);
+    private static void writeInterface(Level level, String msg, boolean append) {
+        writeInterface(level, msg, append, null);
+    }
+    private static void writeInterface(Level level, String msg, boolean append, Throwable error) {
+        write(level, msg, append, error);
         try {
             if (webInterfaceComm != null) {
-            	webInterfaceComm.setSourceValue("verbose", read());
+                webInterfaceComm.setSourceValue("verbose", read());
             }
         }
         catch (Exception e) {
@@ -68,13 +76,25 @@ implements FetchBot {
     }
 
     // Public Static Methods
-    public static void clear() {
+    static {
         console = new Console();
+        javaLogger = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+        javaLoggerHandlerFormatter = new SimpleFormatter();
+        final java.util.logging.Logger javaRootLogger = java.util.logging.Logger.getLogger("");
+        final Handler[] handlers = javaRootLogger.getHandlers();
+        if (handlers[0] instanceof ConsoleHandler) {
+            javaRootLogger.removeHandler(handlers[0]);
+        }
+    }
+    public static void clear() {
         console.title("--- Fetch Bot ---", "https://cshadd.github.io/fetch-bot/");
         File input = null;
         try {
             input = new File(LOG_PATH);
             FileUtils.deleteQuietly(input);
+            javaLoggerHandler = new FileHandler(LOG_PATH);
+            javaLoggerHandler.setFormatter(javaLoggerHandlerFormatter);
+            javaLogger.addHandler(javaLoggerHandler);
         }
         catch (Exception e) {
             fatalError(e, "There was an unknown issue!");
@@ -82,6 +102,8 @@ implements FetchBot {
         finally { }
     }
     public static void close() {
+        javaLogger.removeHandler(javaLoggerHandler);
+        javaLogger.setLevel(Level.INFO);
         console.promptForExit();
     }
     public static void debug(String msg) {
@@ -91,34 +113,26 @@ implements FetchBot {
         if (debug) {
             msg = TAG + " [DEBUG] " + msg;
             console.println(msg);
-            writeInterface(msg, append);
+            javaLogger.setLevel(Level.INFO);
+            javaLogger.info(msg);
+            writeInterface(Level.FINER, msg, append);
         }
     }
-    public static void error(String msg) {
-        error(msg, true);
-    }
     public static void error(Throwable e, String msg) {
-        final StringWriter errors = new StringWriter();
-        e.printStackTrace(new PrintWriter(errors));
-        error(errors + "\n" + msg);
+        error(e, msg);
     }
-    public static void error(String msg, boolean append) {
+    public static void error(Throwable e, String msg, boolean append) {
         msg = TAG + " [ERROR (SAFELY CAUGHT)] " + msg;
         console.println("\u001B[331" + msg + "\nPlease report this issue to the developers!\u001B[0m");
-        writeInterface(msg, append);
-    }
-    public static void fatalError(String msg) {
-        fatalError(msg, true);
+        writeInterface(Level.SEVERE, msg, append, e);
     }
     public static void fatalError(Throwable e, String msg) {
-        final StringWriter errors = new StringWriter();
-        e.printStackTrace(new PrintWriter(errors));
-        fatalError(errors + "\n" + msg);
+        fatalError(e, msg, true);
     }
-    public static void fatalError(String msg, boolean append) {
+    public static void fatalError(Throwable e, String msg, boolean append) {
         msg = TAG + " [FATAL ERROR (SAFELY CAUGHT)] " + msg;
         console.println("\u001B[31m" + msg + "\nPlease report this issue to the developers!\u001B[0m");
-        write(msg, append);
+        write(Level.SEVERE, msg, append, e);
     }
     public static void info(String msg) {
         info(msg, true);
@@ -126,7 +140,7 @@ implements FetchBot {
     public static void info(String msg, boolean append) {
         msg = TAG + " [INFO] " + msg;
         console.println(msg);
-        writeInterface(msg, append);
+        writeInterface(Level.INFO, msg, append);
     }
     public static void setWebInterfaceCommunications(WebInterfaceCommunication comm) {
         webInterfaceComm = comm;
@@ -135,16 +149,14 @@ implements FetchBot {
         debug = true;
     }
     public static void warn(String msg) {
-        warn(msg, true);
+        warn(null, msg);
     }
     public static void warn(Throwable e, String msg) {
-        final StringWriter errors = new StringWriter();
-        e.printStackTrace(new PrintWriter(errors));
-        warn(errors + "\n" + msg);
+        warn(e, msg, true);
     }
-    public static void warn(String msg, boolean append) {
+    public static void warn(Throwable e, String msg, boolean append) {
         msg = TAG + " [WARN] " + msg;
         console.println("\u001B[33m" + msg + "\u001B[0m");
-        writeInterface(msg, append);
+        writeInterface(Level.WARNING, msg, append, e);
     }
 }
