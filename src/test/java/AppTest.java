c584bb33ab16240eec2138582d757f4ba17aad99
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-05-03 15:05:05 -0700

    Fixing pathfinding

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,28 +1,9 @@
 import org.junit.Test;
-import io.github.cshadd.fetch_bot.controllers.*;
-import io.github.cshadd.fetch_bot.io.*;
-import io.github.cshadd.fetch_bot.util.*;
 
 // Main
 public class AppTest {
     @Test
     public void test() {
         System.out.println("Tested.");
-        
-        
-        final PathfindController p = new PathfindControllerImpl();
-        p.reset();
-        System.out.println(p);
-
-        p.goNext();
-        System.out.println(p);
-        p.blockNext();
-        System.out.println(p);
-        p.rotateLeft();
-        System.out.println(p);
-        p.rotateLeft();
-        p.goNext();
-        System.out.println(p);
-        
     }
 }
