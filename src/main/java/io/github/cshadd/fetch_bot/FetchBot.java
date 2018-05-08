d6a44e5d1d04950cde3d8d04b4637eea2f76ec1b
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-05-08 10:52:09 -0700

    Javadoc

--- src/main/java/io/github/cshadd/fetch_bot/FetchBot.java
+++ src/main/java/io/github/cshadd/fetch_bot/FetchBot.java
@@ -1,136 +1,178 @@
 package io.github.cshadd.fetch_bot;
 
 // Main
+/**
+* Interface FetchBot is the root of the project.
+* All project objects implement this interface.
+*
+* @since v1.0.0
+*/
 public abstract interface FetchBot {
     /* Template:
-        * Fields
-            -Private Constant Instance/Property Fields
-            -Protected Constant Instance/Property Fields
-            -Public Constant Instance/Property Fields
-            -Private Static Instance/Property Fields
-            -Protected Static Instance/Property Fields
-            -Public Static Instance/Property Fields
-            -Private Final Instance/Property Fields
-            -Protected Final Instance/Property Fields
-            -Public Final Instance/Property Fields
-            -Private Instance/Property Fields
-            -Protected Instance/Property Fields
-            -Public Instance/Property Fields
-        * Constructors
-            -Private Constructors
-            -Protected Constructors
-            -Public Constructors
-        * Factories
-            -Private Factories
-            -Protected Factories
-            -Public Factories
-        * Nested Classes
-            -Private Static Abstract Nested Classes
-            -Protected Static Abstract Nested Classes
-            -Public Static Abstract Nested Classes
-            -Private Abstract Nested Classes
-            -Protected Abstract Nested Classes
-            -Public Abstract Nested Classes
-            -Private Static Final Nested Classes
-            -Protected Static Final Nested Classes
-            -Public Static Final Nested Classes
-            -Private Static Nested Classes
-            -Protected Static Nested Classes
-            -Public Static Nested Classes
-            -Private Final Nested Classes
-            -Protected Final Nested Classes
-            -Public Final Nested Classes
-            -Private Nested Classes
-            -Protected Nested Classes
-            -Public Nested Classes
-        * Methods
-            -Protected Abstract Property Accessor Methods
-            -Public Abstract Property Accessor Methods
-            -Protected Abstract Property Accessor Methods (Overrided)
-            -Public Abstract Property Accessor Methods (Overrided)
-            -Protected Abstract Property Mutator Methods
-            -Public Abstract Property Mutator Methods
-            -Protected Abstract Property Mutator Methods (Overrided)
-            -Public Abstract Property Mutator Methods (Overrided)
-            -Protected Abstract Helper Methods
-            -Public Abstract Helper Methods
-            -Protected Abstract Helper Methods (Overrided)
-            -Public Abstract Helper Methods (Overrided)
-            -Protected Abstract Methods
-            -Public Abstract Methods
-            -Protected Abstract Methods (Overrided)
-            -Public Abstract Methods (Overrided)
-            -Private Static Final Property Accessor Methods
-            -Protected Static Final Property Accessor Methods
-            -Public Static Final Property Accessor Methods
-            -Private Static Final Property Mutator Methods
-            -Protected Static Final Property Mutator Methods
-            -Public Static Final Property Mutator Methods
-            -Private Static Final Helper Methods
-            -Protected Static Final Helper Methods
-            -Public Static Final Helper Methods
-            -Private Static Final Methods
-            -Protected Static Final Methods
-            -Public Static Final Methods
-            -Private Static Property Accessor Methods
-            -Protected Static Property Accessor Methods
-            -Public Static Property Accessor Methods
-            -Private Static Property Mutator Methods
-            -Protected Static Property Mutator Methods
-            -Public Static Property Mutator Methods
-            -Private Static Helper Methods
-            -Protected Static Helper Methods
-            -Public Static Helper Methods
-            -Private Static Methods
-            -Protected Static Methods
-            -Public Static Methods
-            -Private Final Property Accessor Methods
-            -Protected Final Property Accessor Methods
-            -Public Final Property Accessor Methods
-            -Private Final Property Accessor Methods (Overrided)
-            -Protected Final Property Accessor Methods (Overrided)
-            -Public Final Property Accessor Methods (Overrided)
-            -Private Final Property Mutator Methods
-            -Protected Final Property Mutator Methods
-            -Public Final Property Mutator Methods
-            -Private Final Property Mutator Methods (Overrided)
-            -Protected Final Property Mutator Methods (Overrided)
-            -Public Final Property Mutator Methods (Overrided)
-            -Private Final Helper Methods
-            -Protected Final Helper Methods
-            -Public Final Helper Methods
-            -Private Final Helper Methods (Overrided)
-            -Protected Final Helper Methods (Overrided)
-            -Public Final Helper Methods (Overrided)
-            -Private Final Methods
-            -Protected Final Methods
-            -Public Final Methods
-            -Private Final Methods (Overrided)
-            -Protected Final Methods (Overrided)
-            -Public Final Methods (Overrided)
-            -Private Property Accessor Methods
-            -Protected Property Accessor Methods
-            -Public Property Accessor Methods
-            -Private Property Accessor Methods (Overrided)
-            -Protected Property Accessor Methods (Overrided)
-            -Public Property Accessor Methods (Overrided)
-            -Private Property Mutator Methods
-            -Protected Property Mutator Methods
-            -Public Property Mutator Methods
-            -Private Property Mutator Methods (Overrided)
-            -Protected Property Mutator Methods (Overrided)
-            -Public Property Mutator Methods (Overrided)
-            -Private Helper Methods
-            -Protected Helper Methods
-            -Public Helper Methods
-            -Private Helper Methods (Overrided)
-            -Protected Helper Methods (Overrided)
-            -Public Helper Methods (Overrided)
-            -Private Methods
-            -Protected Methods
-            -Public Methods
-            -Private Methods (Overrided)
-            -Protected Methods (Overrided)
-            -Public Methods (Overrided)
-    */
+     * Fields
+
+         -Private Constant Instance/Property Fields
+         -Protected Constant Instance/Property Fields
+         -Public Constant Instance/Property Fields
+
+         -Private Static Instance/Property Fields
+         -Protected Static Instance/Property Fields
+         -Public Static Instance/Property Fields
+
+         -Private Final Instance/Property Fields
+         -Protected Final Instance/Property Fields
+         -Public Final Instance/Property Fields
+
+         -Private Instance/Property Fields
+         -Protected Instance/Property Fields
+         -Public Instance/Property Fields
+
+     * Constructors
+
+         -Private Constructors
+         -Protected Constructors
+         -Public Constructors
+
+     * Factories
+
+         -Private Factories
+         -Protected Factories
+         -Public Factories
+
+     * Nested Classes
+
+         -Private Static Abstract Nested Classes
+         -Protected Static Abstract Nested Classes
+         -Public Static Abstract Nested Classes
+
+         -Private Abstract Nested Classes
+         -Protected Abstract Nested Classes
+         -Public Abstract Nested Classes
+
+         -Private Static Final Nested Classes
+         -Protected Static Final Nested Classes
+         -Public Static Final Nested Classes
+
+         -Private Static Nested Classes
+         -Protected Static Nested Classes
+         -Public Static Nested Classes
+
+         -Private Final Nested Classes
+         -Protected Final Nested Classes
+         -Public Final Nested Classes
+
+         -Private Nested Classes
+         -Protected Nested Classes
+         -Public Nested Classes
+
+     * Methods
+
+         -Protected Abstract Property Accessor Methods
+         -Public Abstract Property Accessor Methods
+         -Protected Abstract Property Accessor Methods (Overrided)
+         -Public Abstract Property Accessor Methods (Overrided)
+
+         -Protected Abstract Property Mutator Methods
+         -Public Abstract Property Mutator Methods
+         -Protected Abstract Property Mutator Methods (Overrided)
+         -Public Abstract Property Mutator Methods (Overrided)
+
+         -Protected Abstract Helper Methods
+         -Public Abstract Helper Methods
+         -Protected Abstract Helper Methods (Overrided)
+         -Public Abstract Helper Methods (Overrided)
+
+         -Protected Abstract Methods
+         -Public Abstract Methods
+         -Protected Abstract Methods (Overrided)
+         -Public Abstract Methods (Overrided)
+
+         -Private Static Final Property Accessor Methods
+         -Protected Static Final Property Accessor Methods
+         -Public Static Final Property Accessor Methods
+
+         -Private Static Final Property Mutator Methods
+         -Protected Static Final Property Mutator Methods
+         -Public Static Final Property Mutator Methods
+
+         -Private Static Final Helper Methods
+         -Protected Static Final Helper Methods
+         -Public Static Final Helper Methods
+
+         -Private Static Final Methods
+         -Protected Static Final Methods
+         -Public Static Final Methods
+
+         -Private Static Property Accessor Methods
+         -Protected Static Property Accessor Methods
+         -Public Static Property Accessor Methods
+
+         -Private Static Property Mutator Methods
+         -Protected Static Property Mutator Methods
+         -Public Static Property Mutator Methods
+
+         -Private Static Helper Methods
+         -Protected Static Helper Methods
+         -Public Static Helper Methods
+
+         -Private Static Methods
+         -Protected Static Methods
+         -Public Static Methods
+
+         -Private Final Property Accessor Methods
+         -Protected Final Property Accessor Methods
+         -Public Final Property Accessor Methods
+         -Private Final Property Accessor Methods (Overrided)
+         -Protected Final Property Accessor Methods (Overrided)
+         -Public Final Property Accessor Methods (Overrided)
+
+         -Private Final Property Mutator Methods
+         -Protected Final Property Mutator Methods
+         -Public Final Property Mutator Methods
+         -Private Final Property Mutator Methods (Overrided)
+         -Protected Final Property Mutator Methods (Overrided)
+         -Public Final Property Mutator Methods (Overrided)
+
+         -Private Final Helper Methods
+         -Protected Final Helper Methods
+         -Public Final Helper Methods
+         -Private Final Helper Methods (Overrided)
+         -Protected Final Helper Methods (Overrided)
+         -Public Final Helper Methods (Overrided)
+
+         -Private Final Methods
+         -Protected Final Methods
+         -Public Final Methods
+         -Private Final Methods (Overrided)
+         -Protected Final Methods (Overrided)
+         -Public Final Methods (Overrided)
+
+         -Private Property Accessor Methods
+         -Protected Property Accessor Methods
+         -Public Property Accessor Methods
+         -Private Property Accessor Methods (Overrided)
+         -Protected Property Accessor Methods (Overrided)
+         -Public Property Accessor Methods (Overrided)
+
+         -Private Property Mutator Methods
+         -Protected Property Mutator Methods
+         -Public Property Mutator Methods
+         -Private Property Mutator Methods (Overrided)
+         -Protected Property Mutator Methods (Overrided)
+         -Public Property Mutator Methods (Overrided)
+
+         -Private Helper Methods
+         -Protected Helper Methods
+         -Public Helper Methods
+         -Private Helper Methods (Overrided)
+         -Protected Helper Methods (Overrided)
+         -Public Helper Methods (Overrided)
+
+         -Private Methods
+         -Protected Methods
+         -Public Methods
+         -Private Methods (Overrided)
+         -Protected Methods (Overrided)
+         -Public Methods (Overrided)
+ */
 }
