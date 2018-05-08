089f0d47f5715183523ef6ad201809fd1c53e552
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-05-08 10:18:43 -0700

    Fixing branch

--- src/www/processRobot.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $myFile = "comms/toRobot.json";
-    $fh = fopen($myFile, "w") or die("Can't use file...");
-    $stringData = $_POST["data"];
-    fwrite($fh, $stringData);
-    fclose($fh)
-?>
