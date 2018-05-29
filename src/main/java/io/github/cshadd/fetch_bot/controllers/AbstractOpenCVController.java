c389a14d3513f893971aea2dbdac5492b4f8ec27
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-05-29 10:08:31 -0700

    Access modifier update

--- src/main/java/io/github/cshadd/fetch_bot/controllers/AbstractOpenCVController.java
+++ src/main/java/io/github/cshadd/fetch_bot/controllers/AbstractOpenCVController.java
@@ -15,6 +15,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
 import org.apache.commons.io.FileUtils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -41,17 +44,17 @@ implements OpenCVController {
     private static final int SIZE = 300;
 
     // Private Final Instance/Property Fields
-    private final VideoCapture camera;
     private final List<String> trackClasses;
     
     // Protected Final Instance/Property Fields
+    protected final VideoCapture camera;
     protected final int cameraPort;
     protected final Thread cameraThread;
     protected final CameraThread cameraRunnable;
     
     // Private Instance/Property Fields
-    private BufferedImage buffer;
-    private Mat cameraFrame;
+    protected BufferedImage buffer;
+    protected Mat cameraFrame;
     private String capturedLabel;
     private Mat det;
     private int endX;
@@ -59,15 +62,15 @@ implements OpenCVController {
     private Net net;
     private int startX;
     private int startY;
-    private JComponent terminalContent;
-    private JLabel terminalLabelCam;
-    private JLabel terminalLabelCamFilter;
-    private JLabel terminalLabelStatus;
-    private JLabel terminalLabelTrack;
-    private JLayeredPane terminalLayerPane;
 
     // Protected Instance/Property Fields
+    protected JComponent terminalContent;
     protected JFrame terminalFrame;
+    protected JLabel terminalLabelCam;
+    protected JLabel terminalLabelCamFilter;
+    protected JLabel terminalLabelStatus;
+    protected JLabel terminalLabelTrack;
+    protected JLayeredPane terminalLayerPane;
     protected String trackClass;
     protected boolean trackClassFound;
     
@@ -76,23 +79,23 @@ implements OpenCVController {
     throws OpenCVControllerException {
         this(DEFAULT_CAMERA_PORT);
     }
-    protected AbstractOpenCVController(int cameraPort)
+    protected AbstractOpenCVController(int newCameraPort)
     throws OpenCVControllerException {
         super();
-        buffer = null;
-        this.cameraPort = cameraPort;
-        camera = new VideoCapture(this.cameraPort);
-        cameraFrame = new Mat();
-        cameraRunnable = new CameraThread();
-        cameraThread = new Thread(cameraRunnable);
-        capturedLabel = "";
-        det = null;
-        endX = 0;
-        endY = 0;
+        this.buffer = null;
+        this.cameraPort = newCameraPort;
+        this.camera = new VideoCapture(this.cameraPort);
+        this.cameraFrame = new Mat();
+        this.cameraRunnable = new CameraThread();
+        this.cameraThread = new Thread(this.cameraRunnable);
+        this.capturedLabel = "";
+        this.det = null;
+        this.endX = 0;
+        this.endY = 0;
         try {
-            net = Dnn.readNetFromCaffe(DEPLOY_PROTOTXT_TXT_FILE, DEPLOY_CAFFEMODEL_FILE);
+            this.net = Dnn.readNetFromCaffe(DEPLOY_PROTOTXT_TXT_FILE, DEPLOY_CAFFEMODEL_FILE);
             final File trackClassesInput = new File(DEPLOY_TRACK_CLASSES_FILE);
-            trackClasses = FileUtils.readLines(trackClassesInput, "UTF-8");
+            this.trackClasses = FileUtils.readLines(trackClassesInput, "UTF-8");
         }
         catch (IOException e) {
             throw new OpenCVControllerException("Could not read neural network files.", e);
@@ -101,60 +104,60 @@ implements OpenCVController {
             throw new OpenCVControllerException("Unknown issue.", e);
         }
         finally { /* */ }
-        trackClass = "None";
-        trackClassFound = false;
-        startX = 0;
-        startY = 0;
+        this.trackClass = "None";
+        this.trackClassFound = false;
+        this.startX = 0;
+        this.startY = 0;
 
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             // Public Methods (Overrided)
             @Override
             public void run() {
-                terminalLabelCam = new JLabel();
-                terminalLabelCam.setVerticalAlignment(JLabel.TOP);
-                terminalLabelCam.setHorizontalAlignment(JLabel.LEFT);
-                terminalLabelCam.setOpaque(false);
-                terminalLabelCam.setBounds(0, 0, SCENE_W, SCENE_H);
+                AbstractOpenCVController.this.terminalLabelCam = new JLabel();
+                AbstractOpenCVController.this.terminalLabelCam.setVerticalAlignment(SwingConstants.TOP);
+                AbstractOpenCVController.this.terminalLabelCam.setHorizontalAlignment(SwingConstants.LEFT);
+                AbstractOpenCVController.this.terminalLabelCam.setOpaque(false);
+                AbstractOpenCVController.this.terminalLabelCam.setBounds(0, 0, SCENE_W, SCENE_H);
                 
-                terminalLabelCamFilter = new JLabel();
-                terminalLabelCamFilter.setVerticalAlignment(JLabel.TOP);
-                terminalLabelCamFilter.setHorizontalAlignment(JLabel.LEFT);
-                terminalLabelCamFilter.setOpaque(true);
-                terminalLabelCamFilter.setBackground(new Color(255, 0, 0, 150));
-                terminalLabelCamFilter.setBounds(0, 0, SCENE_W, SCENE_H);
+                AbstractOpenCVController.this.terminalLabelCamFilter = new JLabel();
+                AbstractOpenCVController.this.terminalLabelCamFilter.setVerticalAlignment(SwingConstants.TOP);
+                AbstractOpenCVController.this.terminalLabelCamFilter.setHorizontalAlignment(SwingConstants.LEFT);
+                AbstractOpenCVController.this.terminalLabelCamFilter.setOpaque(true);
+                AbstractOpenCVController.this.terminalLabelCamFilter.setBackground(new Color(255, 0, 0, 150));
+                AbstractOpenCVController.this.terminalLabelCamFilter.setBounds(0, 0, SCENE_W, SCENE_H);
                 
-                terminalLabelStatus = new JLabel();
-                terminalLabelStatus.setVerticalAlignment(JLabel.BOTTOM);
-                terminalLabelStatus.setHorizontalAlignment(JLabel.LEFT);
-                terminalLabelStatus.setOpaque(false);
-                terminalLabelStatus.setBounds(0, 0, SCENE_W, SCENE_H);
+                AbstractOpenCVController.this.terminalLabelStatus = new JLabel();
+                AbstractOpenCVController.this.terminalLabelStatus.setVerticalAlignment(SwingConstants.BOTTOM);
+                AbstractOpenCVController.this.terminalLabelStatus.setHorizontalAlignment(SwingConstants.LEFT);
+                AbstractOpenCVController.this.terminalLabelStatus.setOpaque(false);
+                AbstractOpenCVController.this.terminalLabelStatus.setBounds(0, 0, SCENE_W, SCENE_H);
 
-                terminalLabelTrack = new JLabel();
-                terminalLabelTrack.setVerticalAlignment(JLabel.TOP);
-                terminalLabelTrack.setHorizontalAlignment(JLabel.LEFT);
-                terminalLabelTrack.setOpaque(false);
-                terminalLabelTrack.setBorder(BorderFactory.createLineBorder(Color.white));
+                AbstractOpenCVController.this.terminalLabelTrack = new JLabel();
+                AbstractOpenCVController.this.terminalLabelTrack.setVerticalAlignment(SwingConstants.TOP);
+                AbstractOpenCVController.this.terminalLabelTrack.setHorizontalAlignment(SwingConstants.LEFT);
+                AbstractOpenCVController.this.terminalLabelTrack.setOpaque(false);
+                AbstractOpenCVController.this.terminalLabelTrack.setBorder(BorderFactory.createLineBorder(Color.white));
                 
-                terminalLayerPane = new JLayeredPane();
-                terminalLayerPane.setPreferredSize(new Dimension(SCENE_W, SCENE_H));
-                terminalLayerPane.add(terminalLabelStatus, 0);
-                terminalLayerPane.add(terminalLabelTrack, 1);
-                terminalLayerPane.add(terminalLabelCamFilter, 2);
-                terminalLayerPane.add(terminalLabelCam, 3);
+                AbstractOpenCVController.this.terminalLayerPane = new JLayeredPane();
+                AbstractOpenCVController.this.terminalLayerPane.setPreferredSize(new Dimension(SCENE_W, SCENE_H));
+                AbstractOpenCVController.this.terminalLayerPane.add(AbstractOpenCVController.this.terminalLabelStatus, 0);
+                AbstractOpenCVController.this.terminalLayerPane.add(AbstractOpenCVController.this.terminalLabelTrack, 1);
+                AbstractOpenCVController.this.terminalLayerPane.add(AbstractOpenCVController.this.terminalLabelCamFilter, 2);
+                AbstractOpenCVController.this.terminalLayerPane.add(AbstractOpenCVController.this.terminalLabelCam, 3);
                 
-                terminalContent = new JPanel();
-                terminalContent.setLayout(new BoxLayout(terminalContent, BoxLayout.PAGE_AXIS));
-                terminalContent.setOpaque(true);
-                terminalContent.add(terminalLayerPane);
+                AbstractOpenCVController.this.terminalContent = new JPanel();
+                AbstractOpenCVController.this.terminalContent.setLayout(new BoxLayout(AbstractOpenCVController.this.terminalContent, BoxLayout.PAGE_AXIS));
+                AbstractOpenCVController.this.terminalContent.setOpaque(true);
+                AbstractOpenCVController.this.terminalContent.add(AbstractOpenCVController.this.terminalLayerPane);
                 
-                terminalFrame = new JFrame("Fetch Bot OpenCVController Terminal");
-                terminalFrame.setContentPane(terminalContent);
-                terminalFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                terminalFrame.setLocationByPlatform(true);
-                terminalFrame.setResizable(false);
+                AbstractOpenCVController.this.terminalFrame = new JFrame("Fetch Bot OpenCVController Terminal");
+                AbstractOpenCVController.this.terminalFrame.setContentPane(AbstractOpenCVController.this.terminalContent);
+                AbstractOpenCVController.this.terminalFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                AbstractOpenCVController.this.terminalFrame.setLocationByPlatform(true);
+                AbstractOpenCVController.this.terminalFrame.setResizable(false);
 
-                terminalFrame.pack();
-                terminalFrame.setVisible(true);
+                AbstractOpenCVController.this.terminalFrame.pack();
+                AbstractOpenCVController.this.terminalFrame.setVisible(true);
             }
         });
     }
@@ -167,27 +170,27 @@ implements OpenCVController {
 
         // Public Constructors
         public CameraThread() {
-            running = false;
+            this.running = false;
         }
         
         // Public Methods
         public void terminate() {
-            buffer.flush();
-            running = false;
+            AbstractOpenCVController.this.buffer.flush();
+            this.running = false;
         }
         
         // Public Methods (Overrided)
         @Override
         public void run() {
-            if (camera.isOpened()) {
-                if (camera.read(cameraFrame)) {
-                    running = true;
-                    while (running) {
-                        if (camera.read(cameraFrame)) {
-                            detections(cameraFrame);
+            if (AbstractOpenCVController.this.camera.isOpened()) {
+                if (AbstractOpenCVController.this.camera.read(AbstractOpenCVController.this.cameraFrame)) {
+                    this.running = true;
+                    while (this.running) {
+                        if (AbstractOpenCVController.this.camera.read(AbstractOpenCVController.this.cameraFrame)) {
+                            detections(AbstractOpenCVController.this.cameraFrame);
                         }
                     }
-                    camera.release();
+                    AbstractOpenCVController.this.camera.release();
                 }
             }
         }
@@ -216,18 +219,18 @@ implements OpenCVController {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    // Private Methods
-    private void detections(Mat mat)
+    // Protected Methods
+    protected void detections(Mat mat)
     {
-        buffer = matToBufferedImage(mat);
-        final ImageIcon img = new ImageIcon(buffer);
+        this.buffer = matToBufferedImage(mat);
+        final ImageIcon img = new ImageIcon(this.buffer);
         /*
          * Converts the image matrix into a "blob," which is then fed into the Caffe neural network using net.setInput()
          * and detections are output as a matrix in net.forward().
          */
         final Mat blob = Dnn.blobFromImage(mat, SCALE_FACTOR, new Size(SIZE, SIZE), new Scalar(SIZE, SIZE), false, false);
-        net.setInput(blob);
-        det = net.forward();
+        this.net.setInput(blob);
+        this.det = this.net.forward();
         /* 
          * For each detection, there are 7 numbers the matrix det outputs. The first is always 0 for some unknown reason, the second is the object it
          * detects (output as the index from the TrackClasses enum (which is why you shouldn't change the order of the classes)), the third is the confidence
@@ -236,43 +239,43 @@ implements OpenCVController {
          * multiply them by the size of the image to get the actual location of the object with respects to the image. This for loop then draws a
          * rectangle around each detection and text identifying each image.
          */
-        for (int i = 0; i < det.reshape(1, 1).size().width; i += 7)
+        for (int i = 0; i < this.det.reshape(1, 1).size().width; i += 7)
         {
-            final int index = (int)det.reshape(1, 1).get(0, i + 1)[0];
-            final double con = det.reshape(1, 1).get(0, i + 2)[0];
+            final int index = (int)this.det.reshape(1, 1).get(0, i + 1)[0];
+            final double con = this.det.reshape(1, 1).get(0, i + 2)[0];
             final String capturedTrackClass;
             final int fullCon = (int)(100*con);
             
-            startX = (int)(det.reshape(1, 1).get(0, i + 3)[0]*SCENE_W);
-            startY = (int)(det.reshape(1, 1).get(0, i + 4)[0]*SCENE_H);
-            endX = (int)(det.reshape(1, 1).get(0, i + 5)[0]*SCENE_W);
-            endY = (int)(det.reshape(1, 1).get(0, i + 6)[0]*SCENE_H);
+            this.startX = (int)(this.det.reshape(1, 1).get(0, i + 3)[0]*SCENE_W);
+            this.startY = (int)(this.det.reshape(1, 1).get(0, i + 4)[0]*SCENE_H);
+            this.endX = (int)(this.det.reshape(1, 1).get(0, i + 5)[0]*SCENE_W);
+            this.endY = (int)(this.det.reshape(1, 1).get(0, i + 6)[0]*SCENE_H);
             
-            if (index > 0 && index < trackClasses.size()) {
-                capturedTrackClass = trackClasses.get(index);
+            if (index > 0 && index < this.trackClasses.size()) {
+                capturedTrackClass = this.trackClasses.get(index);
             }
             else {
                 capturedTrackClass = "???"; 
             }
             
-            if (fullCon >= CONFIDENCE_LIMIT && capturedTrackClass.equals(trackClass)) {
-                trackClassFound = true;
-                capturedLabel = "<html><p style='color: white;'>" + capturedTrackClass
+            if (fullCon >= CONFIDENCE_LIMIT && capturedTrackClass.equals(this.trackClass)) {
+                this.trackClassFound = true;
+                this.capturedLabel = "<html><p style='color: white;'>" + capturedTrackClass
                         + " [" + fullCon + "%]<br />"
                         + "TARGET</p></html>";
             }
             else {
-                trackClassFound = false;
-                capturedLabel = "<html><p style='color: white;'>" + capturedTrackClass
+                this.trackClassFound = false;
+                this.capturedLabel = "<html><p style='color: white;'>" + capturedTrackClass
                         + " [" + fullCon + "%]</p></html>";
             }
         }
-        terminalLabelCam.setIcon(img);
-        terminalLabelStatus.setText("<html><p style='color: white; font-size: 20px'>&#187; Status: Processing<br />"
-                + "Target: " + trackClass + "<br />"
-                + "Found Target: " + trackClassFound + "<br />"
-                + "Raw I/O Buffer: " + buffer.hashCode() + "</p></html>");
-        terminalLabelTrack.setBounds(startX, startY, endX - startX, endY - startY);
-        terminalLabelTrack.setText(capturedLabel);
+        this.terminalLabelCam.setIcon(img);
+        this.terminalLabelStatus.setText("<html><p style='color: white; font-size: 20px'>&#187; Status: Processing<br />"
+                + "Target: " + this.trackClass + "<br />"
+                + "Found Target: " + this.trackClassFound + "<br />"
+                + "Raw I/O Buffer: " + this.buffer.hashCode() + "</p></html>");
+        this.terminalLabelTrack.setBounds(this.startX, this.startY, this.endX - this.startX, this.endY - this.startY);
+        this.terminalLabelTrack.setText(this.capturedLabel);
     }
 }
