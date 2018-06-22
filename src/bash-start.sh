a5ec7d0a222e981b98a893a0a74031788c849814
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-06-22 11:49:41 -0700

    Trying other socket

--- src/bash-start.sh
+++ src/bash-start.sh
@@ -4,10 +4,8 @@ export DISPLAY=:0
 xset -display :0 s off
 xset -display :0 -dpms
 xset -display :0 s noblank
-sudo pkill unclutter
-unclutter -display :0 &
 sudo pkill java
-/usr/bin/chromium-browser --incognito --start-minimized --kiosk http://localhost:8885/ &
-/usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html &
-java -jar fetch-bot-v2.0.0-alpha.1.jar $1
-sudo pkill chromium
+sudo pkill unclutter
+java -jar fetch-bot-v2.0.0-alpha.1.jar $1 &
+unclutter -display :0 &
+/usr/bin/chromium-browser --incognito --start-maximized --kiosk http://localhost/FetchBot/face.html
--- src/main/java/io/github/cshadd/fetch_bot/References.java
+++ src/main/java/io/github/cshadd/fetch_bot/References.java
@@ -63,7 +63,7 @@ public final class References implements FetchBot {
     /**
      * The Constant HUD_STREAM_HOST.
      */
-    public static final String HUD_STREAM_HOST = "localhost";
+    public static final String HUD_STREAM_HOST = "192.168.1.2";
     
     /**
      * The Constant HUD_STREAM_PORT.
