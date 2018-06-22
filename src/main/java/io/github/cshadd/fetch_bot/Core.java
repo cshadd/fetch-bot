b9274e429b8d7e2672940f442c328cd4c828f4d0
Christian Shadd <christian.v.t.shadd@gmail.com>
2018-06-22 13:52:06 -0700

    Fixed stream

--- src/main/java/io/github/cshadd/fetch_bot/Core.java
+++ src/main/java/io/github/cshadd/fetch_bot/Core.java
@@ -196,7 +196,6 @@ public class Core implements FetchBot {
                     /* */
                 } // Suppressed
                 finally {
-                    delayThread(500);
                 }
             }
         }
